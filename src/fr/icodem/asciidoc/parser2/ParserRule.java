4ea9365b4c18336e17da98e8abfff4da3a6bf1c8
ohanny <olivier.hanny@gmail.com>
2016-01-26 21:03:01 +0100

    Removed trial code

--- src/fr/icodem/asciidoc/parser2/ParserRule.java
+++
@@ -1,6 +0,0 @@
-package fr.icodem.asciidoc.parser2;
-
-@FunctionalInterface
-public interface ParserRule {
-    void parse();
-}
--- src/fr/icodem/asciidoc/parser2/ParserRules.java
+++
@@ -1,6 +0,0 @@
-package fr.icodem.asciidoc.parser2;
-
-public class ParserRules {
-
-    public void add(ParserRule rule) {}
-}
