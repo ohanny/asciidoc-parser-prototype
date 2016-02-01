ea0391f739f518d6b57ba43c005d6c6e83ba2972
ohanny <olivier.hanny@gmail.com>
2016-02-01 14:49:22 +0100

    Changed method signature

--- src/fr/icodem/asciidoc/parser/peg/ParseTreeListener.java
+++ src/fr/icodem/asciidoc/parser/peg/ParseTreeListener.java
@@ -1,7 +1,7 @@
 package fr.icodem.asciidoc.parser.peg;
 
 public interface ParseTreeListener {
-    void characters(char[] characters, int startIndex, int endIndex);
+    void characters(char[] chars, int startIndex, int endIndex);
     void enterNode(String nodeName);
     void exitNode(String nodeName);
 }
