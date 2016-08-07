b6cf7290351c1eb1429ad92f8b29d88c130fdbc1
ohanny <olivier.hanny@gmail.com>
2016-08-07 21:27:35 +0200

    introduced base spock test class

---
+++ test/fr/icodem/asciidoc/parser/peg/BaseSpecification.groovy
@@ -0,0 +1,8 @@
+package fr.icodem.asciidoc.parser.peg
+
+import spock.lang.Specification
+
+
+class BaseSpecification extends Specification {
+
+}
--- test/fr/icodem/asciidoc/parser/peg/BasicFormattedTextSpec.groovy
+++ test/fr/icodem/asciidoc/parser/peg/BasicFormattedTextSpec.groovy
@@ -3,10 +3,8 @@ package fr.icodem.asciidoc.parser.peg
 import fr.icodem.asciidoc.parser.peg.example.FormattedTextParser
 import fr.icodem.asciidoc.parser.peg.runner.ParseRunner
 import fr.icodem.asciidoc.parser.peg.runner.ParsingResult
-import spock.lang.Specification
 
-
-class BasicFormattedTextSpec extends Specification {
+class BasicFormattedTextSpec extends BaseSpecification {
 
     ParsingResult parse(String input) {
         FormattedTextParser parser = new FormattedTextParser();
