45ecf899a938161cba508a462563e554a072ab0b
ohanny <olivier.hanny@gmail.com>
2016-08-07 12:19:39 +0200

    replaced test phrase

--- test/fr/icodem/asciidoc/parser/peg/BasicFormattedTextSpec.groovy
+++ test/fr/icodem/asciidoc/parser/peg/BasicFormattedTextSpec.groovy
@@ -18,68 +18,68 @@ class BasicFormattedTextSpec extends Specification {
 
     def "it should return normal phrase"() {
         given:
-            String input = "normal phrase";
+            String input = "it's a nice day";
 
         when:
             ParsingResult result = parse(input);
 
         then:
-            result.tree == "(formattedText (text n o r m a l   p h r a s e))";
+            result.tree == "(formattedText (text i t ' s   a   n i c e   d a y))";
     }
 
     def "it should return bold phrase"() {
         given:
-            String input = "*bold phrase*";
+            String input = "*it's a nice day*";
 
         when:
             ParsingResult result = parse(input);
 
         then:
-            result.tree == "(formattedText (bold * (text b o l d   p h r a s e) *))";
+            result.tree == "(formattedText (bold * (text i t ' s   a   n i c e   d a y) *))";
     }
 
     def "it should return italic phrase"() {
         given:
-            String input = "_italic phrase_";
+            String input = "_it's a nice day_";
 
         when:
             ParsingResult result = parse(input);
 
         then:
-            result.tree == "(formattedText (italic _ (text i t a l i c   p h r a s e) _))";
+            result.tree == "(formattedText (italic _ (text i t ' s   a   n i c e   d a y) _))";
     }
 
     def "it should return bold word within a phrase"() {
         given:
-        String input = "a *bold* word";
+        String input = "it's a *nice* day";
 
         when:
         ParsingResult result = parse(input);
 
         then:
-        result.tree == "(formattedText (text a  ) (bold * (text b o l d) *) (text   w o r d))";
+        result.tree == "(formattedText (text i t ' s   a  ) (bold * (text n i c e) *) (text   d a y))";
     }
 
     def "it should return italic word within a phrase"() {
         given:
-        String input = "a _bold_ word";
+        String input = "it's a _nice_ day";
 
         when:
         ParsingResult result = parse(input);
 
         then:
-        result.tree == "(formattedText (text a  ) (italic _ (text b o l d) _) (text   w o r d))";
+        result.tree == "(formattedText (text i t ' s   a  ) (italic _ (text n i c e) _) (text   d a y))";
     }
 
     def "it should return bold italic word within a phrase"() {
         given:
-        String input = "a *_bold_* word";
+        String input = "it's a *_nice_* day";
 
         when:
         ParsingResult result = parse(input);
 
         then:
-        result.tree == "(formattedText (text a  ) (bold * (italic _ (text b o l d) _) *) (text   w o r d))";
+        result.tree == "(formattedText (text i t ' s   a  ) (bold * (italic _ (text n i c e) _) *) (text   d a y))";
     }
 
 }
