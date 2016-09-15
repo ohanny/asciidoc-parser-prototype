b3bc757c8ca4a1e866e118d9e434b2cf2fc570d5
ohanny <olivier.hanny@gmail.com>
2016-09-15 11:21:14 +0200

    changed input text

--- test/fr/icodem/asciidoc/parser/block/AdmonitionSpec.groovy
+++ test/fr/icodem/asciidoc/parser/block/AdmonitionSpec.groovy
@@ -1,19 +1,18 @@
 package fr.icodem.asciidoc.parser.block
 
-import fr.icodem.asciidoc.parser.formattedtext.FormattedTextBaseSpecification
 import fr.icodem.asciidoc.parser.peg.runner.ParsingResult
 
-class AdmonitionSpec extends FormattedTextBaseSpecification {
+class AdmonitionSpec extends BlockBaseSpecification {
 
     def "note admonition"() {
         given:
-        String input = "NOTE: xxx";
+        String input = "NOTE: Run `git difftool --tool-help` to see what is available on your system.";
 
         when:
         ParsingResult result = parse(input);
 
         then:
-        result.tree == "(paragraph (admonition N O T E : ) (text I x x) )";
+        result.tree == "(document (content (block (paragraph (admonition N O T E :  ) R u n   ` g i t   d i f f t o o l   - - t o o l - h e l p `   t o   s e e   w h a t   i s   a v a i l a b l e   o n   y o u r   s y s t e m . <EOF>))))";
     }
 
 }
