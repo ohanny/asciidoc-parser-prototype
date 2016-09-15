d25d953a2fc09b3224e3dde364b6f09281c4b2de
ohanny <olivier.hanny@gmail.com>
2016-09-15 11:36:19 +0200

    more tests on admonitions

--- test/fr/icodem/asciidoc/parser/block/AdmonitionSpec.groovy
+++ test/fr/icodem/asciidoc/parser/block/AdmonitionSpec.groovy
@@ -6,13 +6,57 @@ class AdmonitionSpec extends BlockBaseSpecification {
 
     def "note admonition"() {
         given:
-        String input = "NOTE: Run `git difftool --tool-help` to see what is available on your system.";
+        String input = "NOTE: This is a note.";
 
         when:
         ParsingResult result = parse(input);
 
         then:
-        result.tree == "(document (content (block (paragraph (admonition N O T E :  ) R u n   ` g i t   d i f f t o o l   - - t o o l - h e l p `   t o   s e e   w h a t   i s   a v a i l a b l e   o n   y o u r   s y s t e m . <EOF>))))";
+        result.tree == "(document (content (block (paragraph (admonition N O T E :  ) T h i s   i s   a   n o t e . <EOF>))))";
+    }
+
+    def "tip admonition"() {
+        given:
+        String input = "TIP: This is a tip.";
+
+        when:
+        ParsingResult result = parse(input);
+
+        then:
+        result.tree == "(document (content (block (paragraph (admonition T I P :  ) T h i s   i s   a   t i p . <EOF>))))";
+    }
+
+    def "important admonition"() {
+        given:
+        String input = "IMPORTANT: This is important.";
+
+        when:
+        ParsingResult result = parse(input);
+
+        then:
+        result.tree == "(document (content (block (paragraph (admonition I M P O R T A N T :  ) T h i s   i s   i m p o r t a n t . <EOF>))))";
+    }
+
+    def "caution admonition"() {
+        given:
+        String input = "CAUTION: Do this with caution.";
+
+        when:
+        ParsingResult result = parse(input);
+
+        then:
+        result.tree == "(document (content (block (paragraph (admonition C A U T I O N :  ) D o   t h i s   w i t h   c a u t i o n . <EOF>))))";
+    }
+
+    def "warning admonition"() {
+        given:
+        String input = "WARNING: This is a warning.";
+
+        when:
+        ParsingResult result = parse(input);
+
+        then:
+        result.tree == "(document (content (block (paragraph (admonition W A R N I N G :  ) T h i s   i s   a   w a r n i n g . <EOF>))))";
     }
 
 }
