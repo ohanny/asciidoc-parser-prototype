9acf65fad96bfa4fae7f4b61524ca28617e657ff
ohanny <olivier.hanny@gmail.com>
2016-09-16 08:31:35 +0200

    fixed test

--- test/fr/icodem/asciidoc/parser/backend/ParagraphSpec.groovy
+++ test/fr/icodem/asciidoc/parser/backend/ParagraphSpec.groovy
@@ -2,6 +2,7 @@ package fr.icodem.asciidoc.parser.backend
 
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 class ParagraphSpec extends BackendBaseSpecification {
 
@@ -37,17 +38,27 @@ class ParagraphSpec extends BackendBaseSpecification {
         Document doc = transform(input);
 
         then:
-        Element div = doc.select("div#content > div[class=paragraph]").first();
-        div != null;
+        Elements paragraphs = doc.select("div#content > div[class=paragraph]");
+        paragraphs != null;
+        paragraphs.size() == 2;
 
-        div.children().size() == 1;
-        Element p = div.child(0);
-        p.tagName() == "p";
-        p.text() == "Three hours before the Abraham Lincoln " +
-                    "left Brooklyn pier, I received a\n" +
-                    "letter worded as follows:\n" +
-                    "\n" +
-                    "To M. ARONNAX, Professor in the Museum of Paris, " +
-                    "Fifth Avenue Hotel,\n" + "New York.";
+        Element div1 = paragraphs.first();
+        div1 != null;
+
+        div1.children().size() == 1;
+        Element p1 = div1.child(0);
+        p1.tagName() == "p";
+        p1.text() == "Three hours before the Abraham Lincoln " +
+                    "left Brooklyn pier, I received a " +
+                    "letter worded as follows:";
+
+        Element div2 = paragraphs.last();
+        div2 != null;
+
+        div2.children().size() == 1;
+        Element p2 = div2.child(0);
+        p2.tagName() == "p";
+        p2.text() == "To M. ARONNAX, Professor in the Museum of Paris, " +
+                     "Fifth Avenue Hotel, New York.";
     }
 }
