65aa58608ee1f1681fa1aac5d500a116dbc6478d
ohanny <olivier.hanny@gmail.com>
2018-07-24 14:01:19 +0200

    Added test on block title

--- test/fr/icodem/asciidoc/parser/antlr/BlockTitleTest.java
+++ test/fr/icodem/asciidoc/parser/antlr/BlockTitleTest.java
@@ -114,6 +114,31 @@ public class BlockTitleTest extends GrammarTest {
 
                 /* expected */
                 "(document (content (blockTitle . (blockTitleValue F r u i t s . . .) \\n) (block (paragraph K i w i   a n d   a p p l e) (nl \\n))) (bl <EOI>))"
+            },
+            {
+                /* message */
+                "block title before listing",
+
+                /* input */
+                ".Some code\n" +
+                "----\n" +
+                "int i = 0;\n" +
+                "----\n",
+
+                /* expected */
+                "(document (content (blockTitle . (blockTitleValue S o m e   c o d e) \\n) (block (listingBlock (listingBlockDelimiter - - - - \\n) i n t   i   =   0 ; \\n (listingBlockDelimiter - - - - \\n)))) (bl <EOI>))"
+            },
+            {
+                /* message */
+                "block title before list",
+
+                /* input */
+                ".Some fruits\n" +
+                "* Apple\n" +
+                "* Kiwi\n",
+
+                /* expected */
+                "(document (content (blockTitle . (blockTitleValue S o m e   f r u i t s) \\n) (block (list (listItem *   (listItemValue A p p l e) \\n) (listItem *   (listItemValue K i w i) \\n)))) (bl <EOI>))"
             }
         });
 
