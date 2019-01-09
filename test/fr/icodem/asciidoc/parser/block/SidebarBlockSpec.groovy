9e51c503a62d6eb7535407a28599e05a6388612c
ohanny <olivier.hanny@gmail.com>
2019-01-09 19:21:59 +0100

    Added sidebar block test : list in sidebar block

--- test/fr/icodem/asciidoc/parser/block/SidebarBlockSpec.groovy
+++ test/fr/icodem/asciidoc/parser/block/SidebarBlockSpec.groovy
@@ -18,4 +18,20 @@ some content
         result.tree == "(document (content (block (sidebarBlock (sidebarBlockDelimiter * * * * \\n) (paragraph s o m e   c o n t e n t \\n) (sidebarBlockDelimiter * * * * \\n)))) (bl <EOI>))"
     }
 
+    def "sidebar with list"() { // TODO new line required after list ?
+        given:
+        String input = '''\
+****
+* item 1
+* item 2
+
+****
+'''
+        when:
+        ParsingResult result = parse(input)
+
+        then:
+        result.tree == "(document (content (block (sidebarBlock (sidebarBlockDelimiter * * * * \\n) (list (listItem *   (listItemValue i t e m   1) \\n) (listItem *   (listItemValue i t e m   2) \\n)) (bl \\n) (sidebarBlockDelimiter * * * * \\n)))) (bl <EOI>))"
+    }
+
 }
